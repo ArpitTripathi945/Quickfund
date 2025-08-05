8a03937cdf25b384779c9e0d72117cbaa61a897e
Chethan Y G <chethanyg57@gmail.com>
2025-08-05 11:50:21 +0530

    Updated Repayment

--- src/app/user/repayment/repayment.component.ts
+++ src/app/user/repayment/repayment.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-repayment',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './repayment.component.html',
   styleUrl: './repayment.component.css'
 })
 export class RepaymentComponent {
+  form = {
+    loanId: '',
+    amount: null,
+    method: ''
+  };
+
+  onSubmit() {
+    if (this.form.loanId && this.form.amount && this.form.method) {
+      console.log('Repayment Form Submitted:', this.form);
+    }
+  }
 
 }
